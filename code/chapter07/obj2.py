cf84683968d6556b26a5796d300262fc326383f8
Stefan Rolink <stefanrolink@protonmail.com>
2021-12-19 18:15:58 +0100

    OOP stukje erbij, bijna klaar

--- code/chapter07/obj2.py
+++ code/chapter07/obj2.py
@@ -1,23 +1,17 @@
-import math
+from math import pi
 
 
 class Cirkel:
+    r = 0  # De straal is standaard 0, je kan 'm zetten met de constructor.
 
     def __init__(self, r):
-        """
-        Deze gekke functie noemen we de constructor.
-        Als je een nieuwe instantie van dit object aanmaakt
-        met bijv.: x = Cirkel(1)
-        Zal deze funtie dus worden aangeroepen met r=1 in dat geval.
-        """
+        # Deze gekke functie is dus de constructor.
         self.r = r  # Deze regel slaat de waarde van r op als classe waarde
 
     def bereken_oppervlakte(self):
-        # Berekent de oppervlakte op basis van de classe waarde r
-        opp = math.pi * self.r**2
-        return opp
+        # Berekent de oppervlakte op basis van de klasse waarde r
+        return pi * self.r**2
 
     def bereken_omtrek(self):
-        # Berekent de omtrek op basis van de classe waarde r
-        omt = math.pi * self.r * 2
-        return omt
+        # Berekent de omtrek op basis van de klasse waarde r
+        return pi * self.r * 2
